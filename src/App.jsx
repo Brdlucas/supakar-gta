111707905eece401a569b06db8959a9e9b15ec13
Lucas Bourdon <lb.lucasbourdon@gmail.com>
2024-11-02 17:15:48 +0100

    Revert "Insertion de la page d'acceuil ou ce trouve le bouton 'start'"
    This reverts commit 9e1c3192560cee3176a957d3abe6ffe0b31367e6.

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,14 @@ import supakarLogo from "../public/logo.png";
 import loading from "../public/sounds/loading.mp3";
 import select from "../public/sounds/select.wav";
 import spray from "../public/sounds/spray.wav";
-import { useRef } from "react";
 
 function App() {
-  const [isTrue, setIsTrue] = useState(true);
-  const audioRef = useRef(new Audio(loading));
+  // const [instruction, setInstruction] = useState("Click on START to begin.");
+  const audio = new Audio(loading);
   const selectAudio = new Audio(select);
   const sprayAudio = new Audio(spray);
-  const [newcolor, setNewColor] = useState('brown');
-  const [hidden, setHidden] = useState('');
-  const [CarHidden, setCarHidden] = useState('hidden');
-  
-  
+  const [newcolor, setNewColor] = useState('brown')
+  let isTrue = false;
   
   
   function OnClick(e){
@@ -54,27 +50,22 @@ function App() {
 
 
   function LoadingAudio(){
-    const audio = audioRef.current;
 
     if(isTrue === true){
       audio.pause();
-      setIsTrue(false);
+      isTrue = false;
 
     }else{
+      isTrue = true;
       audio.play();
-      setIsTrue(true);
+
     }
   }
-  
 
-
-  function DisplayFirstPageCar(){
-
-   
-      
+  function DisplayFirstPage(){
     return(
       <div
-        className={`h-screen ${CarHidden}`}
+        className="h-screen"
         style={{
           backgroundImage: "url('/cars/gray.png')",
           backgroundSize: "cover",
@@ -84,7 +75,7 @@ function App() {
         <div className="z-0 bg-black h-fit h-screen">
           <div className="h-fit p-5 flex flex-row justify-between w-full">
             <img src={supakarLogo} alt="Supakar Logo" className="w-40" />
-            <button  onClick={LoadingAudio} id='audioLoad'><img src="../public/sound.png" alt="" className="w-10"/></button>
+            <button onClick={LoadingAudio}><img src="../public/sound.png" alt="" className="w-10"/></button>
           </div>
           <div className="w-100 h-[90%] flex flex-col justify-around">
             <div className="w-2/4 m-auto h-2/4 mb-32">
@@ -96,7 +87,7 @@ function App() {
             <input type="button" onClick={OnClick} id="gray"  className="hover:cursor-pointer bg-gray-200 w-12 h-12 rounded-full border-2 border-white"></input>
             <input type="button" onClick={OnClick} id="yellow" className="hover:cursor-pointer bg-yellow-200 w-12 h-12 rounded-full border-2 border-white"></input>
             <input type="button" onClick={OnClick} id="green" className="hover:cursor-pointer bg-green-200 w-12 h-12 rounded-full border-2 border-white"></input>
-            <input type="button" onClick={OnClick} id="purple" className="hover:cursor-pointer bg-purple-200 w-12 h-12 rounded-full border-2 border-white"></input>
+            <input type="button" onClick={OnClick} id="pruple" className="hover:cursor-pointer bg-purple-200 w-12 h-12 rounded-full border-2 border-white"></input>
             <input type="button" onClick={OnClick} id="skyblue" className="hover:cursor-pointer bg-blue-200 w-12 h-12 rounded-full border-2 border-white"></input>
             <input type="button" onClick={OnClick} id="brown" className="hover:cursor-pointer bg-amber-600 w-12 h-12 rounded-full border-2 border-white"></input>
             </ul>
@@ -107,55 +98,24 @@ function App() {
     )
   }
 
-  function DisplayFirstPage(){
-    
-    return(
-      <div>
-      <DisplayFirstPageCar/>
-      <div
-        className={`p-5 flex flex-col items-center justify-center ${hidden} h-screen`}
-        style={{
-          backgroundImage: "url('/cars/gray.png')",
-          backgroundSize: "cover",
-          backgroundRepeat: "no-repeat",
-        }}
-      >
-        <div className="p-10 flex flex-col items-center justify-center shadow-2xl rounded-2xl w-96 bg-white bg-opacity-70">
-          <div className="w-64 mb-10">
-            <img src={supakarLogo} alt="Supakar Logo" />
-          </div>
-          <button
-      onClick={start}
-      className={`bg-blue-500 hover:bg-blue-700 font-bold py-2 px-4 mb-5 rounded`}
-    >
-      START
-    </button>
-          <div className="text-center text-lg transitison duration-300">
-          </div>
-        </div>
-        
-      </div>
-      </div>
-    )
-  }
 
-  const start = () => {
-    LoadingAudio();
-    setTimeout(() => {
-      setHidden('hidden');
-      setCarHidden('block');
-    }, 500);
+  
+  // const start = () => {
+  //   audio.play();
+  //   setTimeout(() => {
+  //     setNewClass('bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mb-5 rounded');
+  //     setInstruction(
+  //       'Supakar est un projet pour l\'apprentissage de React. Il est basé sur le template Vite + React. Pour démarrer, il suffit de lire le fichier "README.md". Bonne session de codage !',
+  //     );
+  //   }, 500);
+  // };
 
-
-    
-  };
 
   return (
     <>
     <DisplayFirstPage />
     </>
   );
-
 }
 
 export default App;
