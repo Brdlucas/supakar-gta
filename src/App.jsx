818158d3648e74f58b869eb45d2579b56efb6403
Lucas Bourdon <lb.lucasbourdon@gmail.com>
2024-11-11 22:14:53 +0100

    suppression de la page avec bouton - Accueil sur le customizer directement

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,8 @@ function App() {
   const audioRef = useRef(new Audio(loading));
   const sprayAudio = new Audio(spray);
   const selectAudio = new Audio(select);
-  const [hidden, setHidden] = useState("");
   const [isTrue, setIsTrue] = useState(false);
   const [newcolor, setNewColor] = useState("brown");
-  const [CarHidden, setCarHidden] = useState("hidden");
   const InputCss =
     "hover:cursor-pointer max-sm:w-8 max-sm:h-8 w-12 h-12 rounded-full border-2 border-white";
 
@@ -48,11 +46,6 @@ function App() {
     }, 300);
   }
 
-  const startAudio = () => {
-    audioRef.current.play();
-    setIsTrue(true);
-  };
-
   const IsSoundPlaying = () => {
     if (isTrue) {
       audioRef.current.pause();
@@ -63,10 +56,10 @@ function App() {
     setIsTrue(!isTrue);
   };
 
-  function DisplayFirstPageCar() {
+  function DisplayPage() {
     return (
       <div
-        className={`h-screen ${CarHidden}`}
+        className={`h-screen`}
         style={{
           backgroundImage: "url('/cars/gray.png')",
           backgroundSize: "cover",
@@ -137,45 +130,9 @@ function App() {
     );
   }
 
-  function DisplayFirstPage() {
-    return (
-      <div>
-        <DisplayFirstPageCar />
-        <div
-          className={`p-5 flex flex-col items-center justify-center ${hidden} h-screen`}
-          style={{
-            backgroundImage: "url('/cars/gray.png')",
-            backgroundSize: "cover",
-            backgroundRepeat: "no-repeat",
-          }}
-        >
-          <div className="p-10 flex flex-col items-center justify-center shadow-2xl rounded-2xl w-96 max-sm:w-72 bg-white bg-opacity-70">
-            <div className="w-64 mb-10">
-              <img src={supakarLogo} alt="Supakar Logo" />
-            </div>
-            <button
-              onClick={start}
-              className={`bg-blue-500 hover:bg-blue-700 font-bold py-2 px-4 mb-5 text-white rounded`}
-            >
-              START
-            </button>
-          </div>
-        </div>
-      </div>
-    );
-  }
-
-  const start = () => {
-    setTimeout(() => {
-      startAudio();
-      setHidden("hidden");
-      setCarHidden("block");
-    }, 500);
-  };
-
   return (
     <>
-      <DisplayFirstPage />
+      <DisplayPage />
     </>
   );
 }
